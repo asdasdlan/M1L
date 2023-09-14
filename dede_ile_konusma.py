53833ce6921480ccb2844ac279af15bd60d62a92
asdasdlan <145045791+asdasdlan@users.noreply.github.com>
2023-09-14 20:13:44 +0300

    Create dede_ile_konusma.py

---
+++ dede_ile_konusma.py
@@ -0,0 +1,17 @@
+meme_dict = {
+            "LOL" : "komik bir şeye verilen cevap"
+            "CRINGE" : "garip ya da utandırıcı bir şey"
+            "ROFL" : "bir şakaya karşılık cevap"
+            "SHEESH" : "onaylamamak"
+            "CREEPY" : "korkunç"
+            "AGGRO" : "agresifleşmek/sinirlenmek"
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+            
+            
+if word in meme_dict.keys():
+    print(meme_dict[word])
+
+else:
+    print("dede onu ben de bilmiyorum.")
